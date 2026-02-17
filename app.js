8f40e28b4a4490575649afa1bffaedd3c3b9ba4c
Pedrohac7 <pedrohac2005@live.com>
2026-02-17 18:55:49 -0300

    Feat(App): Cria app.js
    ele não sobe o servidor apnas configura

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const routes = require('./src/routes/routes');
+const authRoutes = require('./src/routes/auth.routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', routes);
+app.use('/api/auth', authRoutes);
+
+module.exports = app;
