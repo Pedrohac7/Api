2fb4b51ea4b68ea9915713e237669278485fb894
Pedrohac7 <pedrohac2005@live.com>
2026-02-17 18:54:37 -0300

    Feat(Index): Cria Index e adiciona JWT no env

---
+++ src/models/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+  }
+);
+
+const db = {};
+
+// Lê todos os arquivos da pasta models
+fs.readdirSync(__dirname)
+  .filter(file => {
+    return file.indexOf('.') !== 0 && file !== 'index.js' && file.endsWith('.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+// Executa as associações
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
