7982f577316f394042259f03df178870bad6a0c9
Pedrohac7 <pedrohac2005@live.com>
2026-02-17 23:54:43 -0300

    Deploy na goma

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const clientsController = require("../controllers/clients");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 const routes = Router();
 
-routes.get("/clients", clientsController.listaClientes);
-routes.post("/clients", clientsController.adicionaCliente);
-routes.get("/clients/:id", clientsController.listaCliente);
-routes.put("/clients/:id", clientsController.editaCliente);
-routes.delete("/clients/:id", clientsController.deletaCliente);
+routes.get("/clients", authMiddleware, clientsController.listaClientes);
+routes.post("/clients", authMiddleware, clientsController.adicionaCliente);
+routes.get("/clients/:id", authMiddleware, clientsController.listaCliente);
+routes.put("/clients/:id", authMiddleware, clientsController.editaCliente);
+routes.delete("/clients/:id", authMiddleware, clientsController.deletaCliente);
 
 module.exports = routes;
