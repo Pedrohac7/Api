727f4ee80ff5d38918b1af36b28e4282d79b9d7a
Pedrohac7 <pedrohac2005@live.com>
2026-03-16 19:45:36 -0300

    fix: move errorHandler middleware after routes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const errorHandler = require("./src/middlewares/errorHandler");
-console.log(__dirname);
 
 const routes = require('./src/routes/routes');
 const authRoutes = require('./src/routes/authRoutes');
@@ -10,9 +9,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(errorHandler);
 
 app.use('/api', routes);
 app.use('/api/auth', authRoutes);
+
+app.use(errorHandler);
 
 module.exports = app;
